ed6c274e8d1dbb035cfd8a39c1f75146205c82fb
Amanda Daly <techdaly@gmail.com>
2016-08-26 21:10:35 -0700

    False start, TS redo

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1 id="site-title">Slice Vote</h1>'
+})
+
+export class AppComponent { }
--- app/main.js
+++ app/main.js
@@ -1,7 +1,5 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic
-      .platformBrowserDynamic()
-      .bootstrapModule(app.AppModule);
-  });
-})(window.app || (window.app = {}));
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_1 = require('./app.module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
