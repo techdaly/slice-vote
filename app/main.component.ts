8d381527267d4819e9e377f415ec00dc86940ace
Amanda Daly <techdaly@gmail.com>
2016-09-01 09:36:19 -0700

    Adding test content to table

--- app/main.component.ts
+++ app/main.component.ts
@@ -6,32 +6,19 @@ import {Component} from '@angular/core'
             <div id="main">
               <table>
                 <tr>
-                  <th> Hi </th>
-                  <th> Hi </th>
+                  <th> Col 1 </th>
+                  <th> Col 2 </th>
                 </tr>
-                <tr>
-                  <td> Hi </td>
-                  <td> Hi </td>
-                </tr>
-                <tr>
-                  <td> Hi </td>
-                  <td> Hi </td>
-                </tr>
-                <tr>
-                  <td> Hi </td>
-                  <td> Hi </td>
-                </tr>
-                <tr>
-                  <td> Hi </td>
-                  <td> Hi </td>
-                </tr>
-                <tr>
-                  <td> Hi </td>
-                  <td> Hi </td>
-                </tr>
-                <tr>
-                  <td> Hi </td>
-                  <td> Hi </td>
+                <tr id="row-1">
+                  <td class="col-1">
+                    <button class="x-button" id="x-1">X</button>
+                    <div class="can-img"></div>
+                    <p class="can-first">First Name</p>
+                    <p class="can-last">Last Name</p>
+                  </td>
+                  <td class="col-2">
+                  <p class="col-2-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </td>
                 </tr>
               </table>
             </div>
